e12dbacb2a19c27286bc17fc15110f95d3c596d8
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-12 18:44:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,29 +15,28 @@ function createEmployeeRecords(records) {
 };
 
 function createTimeInEvent(timeIn){
-  let newEvent = {type: "TimeIn", hour: parseInt(timeIn.substring(11, 15)), date: timeIn.substring(0,10)}
+  let newEvent = {type: "TimeIn", hour: parseInt(timeIn.substring(11, 15)), date: timeIn.substring(0,8)}
   this.timeInEvents.push(newEvent)
   return this
 };
 
 
 function createTimeOutEvent(timeOut){
-  let newEvent = {type: "TimeOut", hour: parseInt(timeOut.substring(11, 15)), date: timeOut.substring(0,10)}
+  let newEvent = {type: "TimeOut", hour: parseInt(timeOut.substring(11, 15)), date: timeOut.substring(0,8)}
   this.timeOutEvents.push(newEvent)
   return this
 };
 
-let hoursWorkedOnDate = function(soughtDate){
-    let inEvent = this.timeInEvents.find(function(e){
-        return e.date === soughtDate
-    })
+function hoursWorkedOnDate(cRecord, date) {
+  let punchIn = cRecord.timeInEvents.find(function(n){
+     return n.date === date})
+     console.log(punchIn)
+  let punchOut = cRecord.timeOutEvents.find(n => {
+     return n.date === date})
 
-    let outEvent = this.timeOutEvents.find(function(e){
-        return e.date === soughtDate
-    })
-
-    return (outEvent.hour - inEvent.hour) / 100
+    return (punchOut.hour - punchIn.hour)/100
 }
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
